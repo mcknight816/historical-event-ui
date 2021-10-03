a8e2e3bb09e419a501700597f2b90a8f361e47b7
mcknight816 <mcknight816@gmail.com>
2021-10-03 10:12:08 -0400

    remove slash

--- src/app/services/config.ts
+++ src/app/services/config.ts
@@ -1,9 +1,10 @@
 export class Config {
   private readonly _api:string;
   constructor(private environment: any) {
+    console.log(environment);
     this._api = environment.api;
   }
   get api(){
-    return this._api.replace("/undefined","");
+    return this._api;
   }
 }
